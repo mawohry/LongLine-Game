fbcfb62277e828808c84ea13ac0a77ba83da921d
mawohry <mawohry3030@gmail.com>
2015-11-25 18:36:48 +0100

    Update header.h

--- header.h
+++ header.h
@@ -3,6 +3,7 @@
 
 /* Return true if grid positions x, y and z hold the same character as p */
 int are_same (int x, int y, int z, char p);
+int has_won(char p);  /* true if player p has won game */
 
 #endif // HEADER_H_
 
