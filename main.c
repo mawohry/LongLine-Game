00d04f62fbcc22032b20760afe67e323345898ef
mawohry <mawohry3030@gmail.com>
2015-11-25 19:08:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+
+ char Board[4][4]; 
+ char PlayerOneName[]; 
+ char PlayerTwoName[]; 
+ float PlayerOneScore; 
+ float PlayerTwoScore; 
+ int Cell;  
+ int ValidMove; 
+ int NoOfMoves; 
+ int GameHasBeenWon; 
+ int GameHasBeenDrawn; 
+ char CurrentSymbol; 
+ char StartSymbol; 
+ char PlayerOneSymbol; 
+ char PlayerTwoSymbol; 
+ char Answer;
+
+int main(int argc, char *argv[])
+{
+  
+  system("PAUSE");	
+  return 0;
+}
 
