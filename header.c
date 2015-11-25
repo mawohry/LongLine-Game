b9fa61a224b9735cb79b10c09372386ff9b60429
mawohry <mawohry3030@gmail.com>
2015-11-25 18:35:46 +0100

    Update header.c

--- header.c
+++ header.c
@@ -6,3 +6,12 @@ int are_same (int x, int y, int z, char p) {
   if ((grid[x] == p) && (grid[y] == p) && (grid[z] == p))   i = 1; else  i = 0;
   return i;
 }
+
+int has_won(char p) {  /* true if player p has won game */
+  int i = 0;
+  i = (are_same(1, 2, 3, p) || are_same(4, 5, 6, p) ||
+       are_same(7, 8, 9, p) || are_same(1, 4, 7, p) ||
+       are_same(2, 5, 8, p) || are_same(3, 6, 9, p) ||
+       are_same(1, 5, 9, p) || are_same(3, 5, 7, p));
+    return i;
+}
