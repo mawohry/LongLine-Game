be37a1b6c27df0fd6df0a707a2ad0fbaecde444a
mawohry <mawohry3030@gmail.com>
2015-11-25 19:37:00 +0100

    Update header.c

--- header.c
+++ header.c
@@ -1,17 +1,17 @@
 #include "header.h"  /* Include the header (not strictly necessary here) */
 
 /* Return true if grid positions x, y and z hold the same character as p */
-int are_same (int x, int y, int z, char p) {
+int are_same (int x, int y, int z, char p,char Board[]) {
   int i = 0;
-  if ((grid[x] == p) && (grid[y] == p) && (grid[z] == p))   i = 1; else  i = 0;
+  if ((Board[x] == p) && (Board[y] == p) && (Board[z] == p))   i = 1; else  i = 0;
   return i;
 }
 
-int has_won(char p) {  /* true if player p has won game */
+int has_won(char p, char Board[]) {  /* true if player p has won game */
   int i = 0;
-  i = (are_same(1, 2, 3, p) || are_same(4, 5, 6, p) ||
-       are_same(7, 8, 9, p) || are_same(1, 4, 7, p) ||
-       are_same(2, 5, 8, p) || are_same(3, 6, 9, p) ||
-       are_same(1, 5, 9, p) || are_same(3, 5, 7, p));
+  i = (are_same(1, 2, 3, p, Board) || are_same(4, 5, 6, p, Board) ||
+       are_same(7, 8, 9, p, Board) || are_same(1, 4, 7, p, Board) ||
+       are_same(2, 5, 8, p, Board) || are_same(3, 6, 9, p, Board) ||
+       are_same(1, 5, 9, p, Board) || are_same(3, 5, 7, p, Board));
     return i;
 }
