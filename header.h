19b3e949ea27ab8a8e1cec844a30e24f34c65674
mawohry <mawohry3030@gmail.com>
2015-11-25 19:07:53 +0100

    Update header.h

--- header.h
+++ header.h
@@ -1,6 +1,13 @@
 #ifndef HEADER_H_   /* Include guard */
 #define HEADER_H_
 
+void DisplayBoard(char Board[][]);
+void ClearBoard(char Board[][]);
+void GetMoveCoordinates(int* Cell);
+int CheckValidMove(int Cell, char Board[][]);
+int CheckXOrOHasWon(char Board[][]);
+char GetWhoStarts();
+
 /* Return true if grid positions x, y and z hold the same character as p */
 int are_same (int x, int y, int z, char p);
 int has_won(char p);  /* true if player p has won game */
